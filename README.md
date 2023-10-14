3f9b4c96c1fb8f32267df5462a6ea4ab24172698
[FadyGamilM] <1700961@eng.asu.edu.eg>
2023-10-14 22:31:46 +0300

    features/add JOI to ensure the validation for my environment

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # TastyTransit
 TastyTransit is a backend system cloning the UberEats application built-in Nest.js with Typescript and GraphQL with PostgreSQL as database.
+
+
+# The packages : 
+- cross-env <br>
+    I used cross-env so I can configure environment variables on the package.json and access them in the src code to trigger some actions based on them 
+
+- @nestjs/config <br>
+    I used it to setup the config file in the app.module and access the configs from anywhere of the application
--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,25 @@ import { ApolloDriver, ApolloDriverConfig } from "@nestjs/apollo"
 import { TypeOrmModule } from "@nestjs/typeorm"
 import { join } from 'path';
 import { RestaurantsModule } from './restaurants/restaurants.module';
-
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from "joi" // we must import joi in this way because it doesn't have a ts compatability 
 @Module({
     imports: [
+        ConfigModule.forRoot({
+            isGlobal: true, // to access the configs from anywhere within the application
+            envFilePath: process.env.ENV === 'dev' ? ".env.dev" : ".env.test",
+            // if we are in prod, we need to ignore the env files at all 
+            ignoreEnvFile: process.env.ENV === "prod" ? true : false,
+            // lets validate my env variables so my app doesn't start if the env vars are not loaded and are not correct 
+            validationSchema: Joi.object({
+                ENV: Joi.string().valid('dev', 'prod').required(),
+                DB_HOST: Joi.required(),
+                DB_USERNAME: Joi.required(),
+                DB_PASSWORD: Joi.required(),
+                DB_NAME: Joi.required(),
+                DB_PORT: Joi.required(),
+            })
+        }),
         GraphQLModule.forRoot<ApolloDriverConfig>({
             driver: ApolloDriver,
             autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
@@ -14,11 +30,12 @@ import { RestaurantsModule } from './restaurants/restaurants.module';
         RestaurantsModule,
         TypeOrmModule.forRoot({
             type: "postgres",
-            host: "localhost",
-            username: "tastytransit",
-            password: "tastytransit",
-            database: "tastytransitdb",
-            port: 5432,
+            host: process.env.DB_HOST,
+            username: process.env.DB_USERNAME,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME,
+            port: +process.env.DB_PORT, // + to convert it into number
+
             synchronize: true, // for auto migration to the current models state 
             logging: true,
         })
