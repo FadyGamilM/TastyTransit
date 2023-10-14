18f655881eb76715b7b18cbb62f50d4ea0db1367
[FadyGamilM] <1700961@eng.asu.edu.eg>
2023-10-14 21:22:22 +0300

    features/connect to postgres instance on docker via TypeOrm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from "@nestjs/graphql"
 import { ApolloDriver, ApolloDriverConfig } from "@nestjs/apollo"
+import { TypeOrmModule } from "@nestjs/typeorm"
 import { join } from 'path';
 import { RestaurantsModule } from './restaurants/restaurants.module';
+
 @Module({
     imports: [
         GraphQLModule.forRoot<ApolloDriverConfig>({
@@ -10,6 +12,16 @@ import { RestaurantsModule } from './restaurants/restaurants.module';
             autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
         }),
         RestaurantsModule,
+        TypeOrmModule.forRoot({
+            type: "postgres",
+            host: "localhost",
+            username: "tastytransit",
+            password: "tastytransit",
+            database: "tastytransitdb",
+            port: 5432,
+            synchronize: true, // for auto migration to the current models state 
+            logging: true,
+        })
     ],
     controllers: [],
     providers: [],
